305ea6b81e71229f759300ccaebb5ce51e243d5b
“jungtaeyeon” <njtest777@gmail.com>
2023-12-30 16:21:59 +0900

    12.30 MemberController insert

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,14 +1,22 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
   Logger logger = LoggerFactory.getLogger(MemberController.class);
   
-  public MemberController(){
-    logger.info("test1-2");
+  public MemberController(){ // test1 브랜치 수정 -1
+  }
+
+  // test1 브랜치 작성 -1
+  @GetMapping
+  public String memberInsert(Map<String, Object> map){
+    return null;
   }
 }
